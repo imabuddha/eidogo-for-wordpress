6c8609292f380e99b1b98ee1392ca7c02d9bc3b5
fwiffo <fwiffo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-01 21:11:02 +0000

    Fixing dim markers
    git-svn-id: https://plugins.svn.wordpress.org/eidogo-for-wordpress/trunk@200838 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-eidogo.php
+++ wp-eidogo.php
@@ -3,7 +3,7 @@
 Plugin Name: EidoGo for WordPress
 Plugin URI: http://www.fortmyersgo.org/eidogo-for-wordpress/
 Description: EidoGo for WordPress makes it easy to embed SGF files in your WordPress-powered blog with the EidoGo SGF viewer and editor.
-Version: 0.8.8
+Version: 0.8.9
 Author: Thomas Schumm
 Author URI: http://www.fortmyersgo.org/
 */
